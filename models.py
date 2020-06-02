486360f15f06451e7bc0fa76cce4eca1ad13efe3
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-02 15:54:58 +0100

    minor changes to auth.py and models.py

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ import datetime
 
 database_name = "players"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-database_path = os.environ.get('DATABASE_URL')
+database_path_local = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = os.environ.get('DATABASE_URL', database_path_local)
 
 db = SQLAlchemy()
 
