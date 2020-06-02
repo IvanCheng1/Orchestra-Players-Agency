55571a5e1c50ff1f17dfee63200a048df8f1f9bd
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-02 17:52:03 +0100

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -3,7 +3,7 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-from .config import auth0_config
+from ..config import auth0_config
 
 
 AUTH0_DOMAIN = auth0_config['AUTH0_DOMAIN']
