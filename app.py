681cc2dd9f7b0e6369f890f5b4d5e6d3033d92f3
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-03 18:37:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -354,5 +354,6 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run(host='127.0.0.1', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(host='127.0.0.1', port=port, debug=True)
